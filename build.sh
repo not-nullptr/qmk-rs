6506b3404e577d2284d4f101018e2534f43f42aa
not-nullptr <needhelpwithrift@gmail.com>
2025-01-04 13:10:10 +0000

    feat: build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-rm -rf .build && SILENT= CONVERT_TO=elite_pi make VERBOSE=true sofle/rev1:nulls_keymap
+rm -rf .build && SILENT= CONVERT_TO=elite_pi make VERBOSE=true ALLOW_WARNINGS=yes CONSOLE_ENABLE=yes sofle/rev1:nulls_keymap

b87fae6217d7b6769bfa19dcfb1998dbe53ad482
not-nullptr <62841684+not-nullptr@users.noreply.github.com>
2025-03-14 21:30:39 +0000

    fix: remove comments

--- rust/crates/keymap/src/screen.rs
+++ rust/crates/keymap/src/screen.rs
@@ -4,7 +4,5 @@ use qmk::{qmk_callback, screen::Screen};
 #[qmk_callback(() -> bool)]
 fn oled_task_user() -> bool {
     Screen::draw_image(0, 0, HELLO_WORLD);
-    Screen::draw_image(0, 0, HELLO_WORLD);
-
     false
 }

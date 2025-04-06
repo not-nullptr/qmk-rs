8e69073370e8ed85e573f773519850fbef0bb72b
not-nullptr <62841684+not-nullptr@users.noreply.github.com>
2025-04-06 21:17:58 +0100

    aww

--- rust/build_wasm.sh
+++ rust/build_wasm.sh
@@ -1,3 +1,3 @@
 cd crates/keymap
-wasm-pack build && rm -rf /mnt/c/Users/nullptr/qmk-wasm/pkg && cp -r pkg /mnt/c/Users/nullptr/qmk-wasm
+wasm-pack build && rm -rf /mnt/c/Users/nullptr/qmk-wasm/src/lib/pkg && cp -r pkg /mnt/c/Users/nullptr/qmk-wasm/src/lib
 cd ../..
